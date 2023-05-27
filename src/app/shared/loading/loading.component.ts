25ba0611b2e24a220690333f9b9764d34c85376c
thiago-anjos <your_email@example.com>
2023-05-27 17:16:56 -0300

    feature - habilitar o loading global ativando o spinner ao entrar ou sair de uma rota ou carregar um modulo lazy loading

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,25 +1,44 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {LoadingService} from './loading.service';
-import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from "@angular/router";
+import { Component, Input, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { LoadingService } from "./loading.service";
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  RouteConfigLoadEnd,
+  RouteConfigLoadStart,
+  Router,
+} from "@angular/router";
 
 @Component({
-  selector: 'loading',
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css']
+  selector: "loading",
+  templateUrl: "./loading.component.html",
+  styleUrls: ["./loading.component.css"],
 })
 export class LoadingComponent implements OnInit {
-
   @Input()
   routing: boolean = false;
 
-  constructor(public loadingService: LoadingService) {
-
-  }
+  constructor(public loadingService: LoadingService, private route: Router) {}
 
   ngOnInit() {
-
+    this.route.events.subscribe((events) => {
+      console.log(events);
+      //console.log(events instanceof NavigationStart);
+      if (
+        events instanceof NavigationStart ||
+        events instanceof RouteConfigLoadStart
+      ) {
+        this.loadingService.loadingOn();
+      } else if (
+        events instanceof NavigationEnd ||
+        events instanceof RouteConfigLoadEnd ||
+        events instanceof NavigationError ||
+        events instanceof NavigationCancel
+      ) {
+        this.loadingService.loadingOff();
+      }
+    });
   }
-
-
 }
